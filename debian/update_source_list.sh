b9169d15864d6ec7621242e78de38788a806011f
Sneaky Koder <sneaky.koder@gmail.com>
2016-10-18 20:26:37 -0700

    Debian sources list added to the installation, and google chrome added

---
+++ debian/update_source_list.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp /etc/apt/sources.list /etc/apt/sources.listbak
+cp debian/sources.list /etc/apt/sources.list
+cat /etc/apt/sources.list
+apt-get install deb-multimedia-keyring
--- debian8_desktop.sh
+++ debian8_desktop.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 clear
 echo "---   Debian 8 installation."
+./debian/update_source_list.sh
 ./generic/install_update.sh
 ./generic/install_basic.sh
 ./debian/install_complementary_debian_8.sh
 ./install_development.sh
-
+./install_google_chrome.sh
 ./install_google_remote_desktop.sh
 ./install_google_talk_plugin.sh
 ./install_google_music_manager.sh
