9c128a37898ea6f6a2e1a0fce5f6f2f58d454b7a
Sneaky Koder <sneaky.koder@gmail.com>
2016-10-20 20:46:09 -0700

    Installation template updated

--- install_package_template.sh
+++ install_package_template.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 pkg_name=""
-update_arg=$1
-update=0
-if [ $update_arg ] && [ "-u" == $update_arg ]; then
-	update=1
-fi
+pkg_ok=""
+pkg_version=""
 echo "---   $pkg_name installation."
 pkg_ok=$(dpkg-query -W --showformat='${Status}\n' $pkg_name|grep "install ok installed")
-echo Checking for $pkg_name: $pkg_ok
-if [ "" == "$pkg_ok" ] || [ $update == "1" ]; then
+pkg_version=$(dpkg-query -W --showformat='${Version}\n' $pkg_name)
+echo Checking for $pkg_name: $pkg_ok $pkg_version
+if [ -z "$pkg_ok" ] || [ -z "$pkg_version" ] || [ "" == "$pkg_ok" ] || [ "" != "$pkg_version" ]; then
 	echo "No $pkg_name. Setting up $pkg_name."
 	cd /tmp
-	wget -O - {}.key | apt-key add -
-	echo "deb " >> /etc/apt/sources.list.d/{}.list
-	aptitude update -y
-	aptitude install $pkg_name
 
-	rm {}.deb
-	curl -O -J -L {}
-	dpkg -i {}
-	apt-get install -f -y
+	# wget -O - {}.key | apt-key add -
+	# echo "deb " >> /etc/apt/sources.list.d/{}.list
+	# aptitude update -y
+	# aptitude install $pkg_name -y
+
+	# rm {}.deb
+	# curl -O -J -L {}
+	# dpkg -i {}
+	# apt-get install -f -y
 fi
