af73933c76c0623d832b30ae676b1b0a9e3e4460
Sneaky Koder <sneaky.koder@gmail.com>
2016-12-17 13:37:21 -0700

    Google chrome installation updated, unstable added

--- install_google_chrome.sh
+++ install_google_chrome.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 pkg_name="google-chrome-stable"
 pkg_ok=""
+pkg_version=""
 echo "---   $pkg_name installation."
 pkg_ok=$(dpkg-query -W --showformat='${Status}\n' $pkg_name|grep "install ok installed")
 pkg_version=$(dpkg-query -W --showformat='${Version}\n' $pkg_name)
@@ -17,6 +18,7 @@ fi
 
 pkg_name="google-chrome-beta"
 pkg_ok=""
+pkg_version=""
 echo "---   $pkg_name installation."
 pkg_ok=$(dpkg-query -W --showformat='${Status}\n' $pkg_name|grep "install ok installed")
 pkg_version=$(dpkg-query -W --showformat='${Version}\n' $pkg_name)
@@ -28,5 +30,22 @@ if [ -z "$pkg_ok" ] || [ "" == "$pkg_ok" ]; then
 	wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 	curl -O -J -L https://dl.google.com/linux/direct/google-chrome-beta_current_amd64.deb
 	dpkg -i google-chrome-beta_current_amd64.deb
+	apt-get install -f -y
+fi
+
+pkg_name="google-chrome-unstable"
+pkg_ok=""
+pkg_version=""
+echo "---   $pkg_name installation."
+pkg_ok=$(dpkg-query -W --showformat='${Status}\n' $pkg_name|grep "install ok installed")
+pkg_version=$(dpkg-query -W --showformat='${Version}\n' $pkg_name)
+echo Checking for $pkg_name: $pkg_ok $pkg_version
+if [ -z "$pkg_ok" ] || [ "" == "$pkg_ok" ]; then
+	echo "No $pkg_name. Setting up $pkg_name."
+	cd /tmp
+	rm google-chrome-unstable_current_amd64.deb
+	wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+	curl -O -J -L https://dl.google.com/linux/direct/google-chrome-unstable_current_amd64.deb
+	dpkg -i google-chrome-unstable_current_amd64.deb
 	apt-get install -f -y
 fi
