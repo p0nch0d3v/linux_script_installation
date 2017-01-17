edc1f6994367fc8804a9679289ed214e917f35ce
Sneaky Koder <sneaky.koder@gmail.com>
2017-01-17 06:59:47 -0700

    Package template updated

--- template_package_installation.sh
+++ template_package_installation.sh
@@ -10,13 +10,13 @@ if [ -z "$pkg_ok" ] || [ -z "$pkg_version" ] || [ "" == "$pkg_ok" ] || [ "" != "
 	echo "No $pkg_name. Setting up $pkg_name."
 	cd /tmp
 
-	# wget -O - {}.key | apt-key add -
-	# echo "deb " >> /etc/apt/sources.list.d/{}.list
+	# wget -O - {key_file}.key | apt-key add -
+	# echo "deb " >> /etc/apt/sources.list.d/{source}.list
 	# aptitude update -y
 	# aptitude install $pkg_name -y
 
-	# rm {}.deb
-	# curl -O -J -L {}
+	# rm {file}.deb
+	# curl -O -J -L {link} -o {file}
 	# dpkg -i {}
 	# apt-get install -f -y
 fi
