2cd01361bed21f62f5fd7a0d91e8bcf2435fb78b
Sneaky Koder <sneaky.koder@gmail.com>
2017-01-20 21:12:01 -0700

    Templated updated, version magament enhanced

--- template_package_installation.sh
+++ template_package_installation.sh
@@ -5,8 +5,9 @@ pkg_version=""
 echo "---   $pkg_name installation."
 pkg_ok=$(dpkg-query -W --showformat='${Status}\n' $pkg_name|grep "install ok installed")
 pkg_version=$(dpkg-query -W --showformat='${Version}\n' $pkg_name)
-echo Checking for $pkg_name: $pkg_ok $pkg_version
-if [ -z "$pkg_ok" ] || [ -z "$pkg_version" ] || [ "" == "$pkg_ok" ] || [ "" != "$pkg_version" ]; then
+echo Checking for $pkg_name: $pkg_ok, Version: $pkg_version
+
+if [ -z "$pkg_ok" ] || [ -z "$pkg_version" ] || [ "" == "$pkg_ok" ] || [[ ! "$pkg_version" =~ "0.0.0" ]]; then
 	echo "No $pkg_name. Setting up $pkg_name."
 	cd /tmp
 
