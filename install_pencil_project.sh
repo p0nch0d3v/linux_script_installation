6e9fc04ccfe4ed8c014b4b39047a971c5a0a5b75
Sneaky Koder <sneaky.koder@gmail.com>
2018-03-06 20:23:10 -0700

    Pencil project installation updated

--- install_pencil_project.sh
+++ install_pencil_project.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-pkg_name="pencil-prototyping"
+pkg_name="pencil"
 pkg_ok=""
 pkg_version=""
 echo "---   $pkg_name installation."
 pkg_ok=$(dpkg-query -W --showformat='${Status}\n' $pkg_name|grep "install ok installed")
 pkg_version=$(dpkg-query -W --showformat='${Version}\n' $pkg_name)
 echo Checking for $pkg_name: $pkg_ok $pkg_version
-if [ -z "$pkg_ok" ] || [ -z "$pkg_version" ] || [ "" == "$pkg_ok" ] || [ "2.0.18" != "$pkg_version" ]; then
+if [ -z "$pkg_ok" ] || [ -z "$pkg_version" ] || [ "" == "$pkg_ok" ] || [ "3.0.4-49" != "$pkg_version" ]; then
  	echo "No $pkg_name. Setting up $pkg_name."
 	cd /tmp
-	rm pencil-prototyping-2.0.18-ubuntu-all.deb
-	curl -O -J -L https://github.com/prikhi/pencil/releases/download/v2.0.18/pencil-prototyping-2.0.18-ubuntu-all.deb
-	dpkg -i pencil-prototyping-2.0.18-ubuntu-all.deb
+	rm Pencil_3.0.4_amd64.deb
+	curl -O -J -L http://pencil.evolus.vn/dl/V3.0.4/Pencil_3.0.4_amd64.deb
+	dpkg -i Pencil_3.0.4_amd64.deb
 	apt-get install -f -y
 fi
