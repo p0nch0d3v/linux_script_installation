415cc680f8b81c4e2faa7d3a4cab157996e28230
Sneaky Koder <sneaky.koder@gmail.com>
2018-03-06 20:44:14 -0700

    Atom installer updates, add repostory for debian based distros.

--- custom/install_atom_io.sh
+++ custom/install_atom_io.sh
@@ -6,11 +6,11 @@ echo "---   $pkg_name installation."
 pkg_ok=$(dpkg-query -W --showformat='${Status}\n' $pkg_name|grep "install ok installed")
 pkg_version=$(dpkg-query -W --showformat='${Version}\n' $pkg_name)
 echo Checking for $pkg_name: $pkg_ok $pkg_version
-if [ -z "$pkg_ok" ] || [ -z "$pkg_version" ] || [ "" == "$pkg_ok" ] || [ "1.10.2" != "$pkg_version" ]; then
+if [ -z "$pkg_ok" ] || [ -z "$pkg_version" ] || [ "" == "$pkg_ok" ]; then
  	echo "No $pkg_name. Setting up $pkg_name."
-	cd /tmp
-	rm atom-amd64.deb
-	curl -O -J -L https://atom-installer.github.com/v1.10.2/atom-amd64.deb -o atom-amd64.deb
-	dpkg -i atom-amd64.deb
-	apt-get install -f -y
+	# cd /tmp
+	curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+	echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list
+	apt-get update
+	apt-get install atom
 fi
